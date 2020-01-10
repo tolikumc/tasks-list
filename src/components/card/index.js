1b5e6e5302d4a0e631a01c8b688bc2bfdf473235
vitaliy <tolikumc@gmail.com>
2020-01-10 15:29:07 +0200

    [Fix]: small issue

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import './index.scss';
-import PropTypes from 'prop-types';
 
 const Card = ({ children }) => {
   return <div className="card">{children}</div>;
-};
-
-Card.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired
 };
 
 export default Card;
